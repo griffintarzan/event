3fcb91c8f8ac62ebf4b28eae22a6c4e7c108e3c5
griffintarzan <60351359+griffintarzan@users.noreply.github.com>
2020-12-31 16:25:06 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const hour = minute * 60;
 const day = hour * 24;
 
 // set the exactly date & times where to subtract current times.
-let userDate = new Date(2021, 1, 1, 0, 0, 0).getTime();
+let userDate = new Date(2021, 2, 1, 0, 0, 0).getTime();
 
 let calculateDate = setInterval(intervalImplementation, second);
 
